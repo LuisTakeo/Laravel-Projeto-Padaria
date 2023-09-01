dc41d7ce2032c0439c7269c246d598c676c5a57e
LuisTakeo <luistakeo@hotmail.com>
2023-08-31 22:21:30 -0300

    Primeiro Commit

---
+++ padoca-vida-loka/app/Http/Controllers/ProdutoController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Produto;
+
+class ProdutoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $produtos = Produto::all();
+
+        foreach($produtos as $produto)
+        {
+            echo"
+                Id: $produto->idProduto <br>
+                Nome: $produto->produto <br>
+                Descrição: $produto->descrProduto <br>
+                Preço: $produto->valor <br>
+                Data: $produto->dtCadastro <br>
+            ";
+        }
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ padoca-vida-loka/app/Produto.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    //
+	protected $table = "tbProduto";
+    protected $fillable = ['idProduto', 'produto', 'descrProduto', 'valor', 'dtCadastro'];
+    public $timestamps = false;
+}
